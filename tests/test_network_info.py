f225203f35a3146f49973b13f9e0e3541c45e122
Adam Caudill <a@cac.sh>
2025-04-02 08:58:50 -0400

    Fix test

--- tests/test_network_info.py
+++ tests/test_network_info.py
@@ -11,4 +11,4 @@ class TestNetworkInfo(TestCase):
     def test_network_info(self):
         res = network_info.network_info("104.28.27.55")
 
-        self.assertEqual("US - CLOUDFLARENET - Cloudflare, Inc.", res)
+        self.assertEqual("US - CLOUDFLARENET", res)
